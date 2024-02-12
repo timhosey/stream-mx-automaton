ab7ce7260a3a316da7ad9b0dacc1e548eedcc8f8
Tim Hosey <insertcointheater@gmail.com>
2024-02-11 16:49:57 -0800

    removed print_r

--- lib/random_song.php
+++ lib/random_song.php
@@ -66,7 +66,7 @@ require_once('getid3/getid3.php');
   // $id3Data['artist'] = $tag['tags']['id3v2']['artist'];
   // $id3Data['title'] = $tag['tags']['id3v2']['title'];
   // $id3Data['album'] = $tag['tags']['id3v2']['album'];
-  print_r($tag);
+  //print_r($tag);
   $id3Data['artist'] = $tag['comments']['artist'];
   $id3Data['title'] = $tag['comments']['title'];
   $id3Data['album'] = $tag['comments']['album'];
